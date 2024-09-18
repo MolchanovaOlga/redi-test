44be665933ca0e7e26dd2006e61dfd197de7d255
MolchanovaOlga <designer.molchanova@gmail.com>
2024-09-18 20:53:10 +0300

    Add mask to form phone

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import { addTimer } from "./timer.js";
 import { currentDay } from "./currentData.js";
 import "./mobilModal.js";
+import "./mask.js";
 
 addTimer();
 currentDay();
---
+++ js/mask.js
@@ -0,0 +1,3 @@
+IMask(document.querySelector(".phone-mask"), {
+  mask: "+{38}(000)000-00-00",
+});
